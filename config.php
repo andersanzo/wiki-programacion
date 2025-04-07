d7f231f600524ff2c45f608f37ff7c5273e01538
andersanzo <andersanzo@gmail.com>
2025-04-07 11:50:05 +0200

    COMENTARIO

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $user = 'root';
 $password = ''; // Actualiza según tu configuración
 $dbname = 'wiki_programacion';
 
-$mysqli = new mysqli($host, $user, $password, $dbname);
+$mysqli = new mysqli($host, $user, $password, $dbname); // Conexión a la base de datos
 
 if ($mysqli->connect_error) {
     die('Error de conexión (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
